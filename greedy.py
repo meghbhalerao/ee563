e9df11d191a5da49102aff12cc8268dae3b7073b
Megh Manoj Bhalerao <megh98@note01.ee.washington.edu>
2022-11-15 04:10:07 +0000

    added greedy - not yet run/debugged

--- greedy.py
+++ greedy.py
@@ -1,5 +1,27 @@
+from submodular_functions.facility_location import facility_location
+
+# greedy for cardinality constrained submodular max - the greedy type can be lazy, random, priority queue greedy, but for now this function only supports the standard greedy algorithm
+def get_max_gain_idx(rem, sol, V, W):
+    max_gain = -float('inf')
+    max_idx = None
+    for idx in rem:
+        idx = set(idx)
+        gain = facility_location(V, sol.union(idx), W) - facility_location(V, sol, W)
+        if gain > max_gain:
+            max_gain = gain
+            max_idx = idx
+    return set(max_idx)
+
+def greedy_max(V_val, V, k, fn = 'FL', W = None, greedy_type = "standard"):
+    if fn == 'FL':
+        assert W is not None # the similarity kernel can't be none if we are using a facility location function
+    sol = set({})
+    while len(sol) < k:
+        rem = V.difference(sol)
+        max_gain_idx = get_max_gain_idx(rem, sol, V, W)
+        sol.union(max_gain_idx)
+
+    print(sol)
 
 
-# greedy for cardinality constrained submodular max
-def greedy_max(f, V, k):
-    A 
+
--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import numpy as np
 import os
 import torch
 from submodular_functions.facility_location import make_kernel
+import wandb
+import sys
 
+#wandb.init()
 use_gpu = True if torch.cuda.is_available() else False
 if use_gpu == False:
     raise ValueError("Can only proceed if gpu is available!")
@@ -12,8 +15,14 @@ dataset = np.loadtxt(data_path, delimiter=",", dtype=float)
 if use_gpu:
     dataset = torch.tensor(dataset).cuda()
 print("shape of the dataset is", dataset.shape)
-
+n_data = dataset.shape[0]
+print(f"number of data points are {n_data}")
 W  = make_kernel(dataset)
 
 print("shape of the symmetric similarity kernel is ", W.shape)
 
+# doing some initializations
+V = np.arange(0, n_data)
+print(V)
+sys.exit()
+greedy_max(dataset, )
